97882450625d5fd076647450ecaa5bedc6f7d73e
Gowrishankar-Chandrasekar <98813206+1g1o0w1r1i@users.noreply.github.com>
2022-03-08 14:18:47 +0530

    Add files via upload

---
+++ Project/3_Implementation/sketch_mar_8_a.ino.ino
@@ -0,0 +1,44 @@
+#define BLYNK_PRINT SwSerial
+#define BlynkSimpleStream
+#define dhtpin 3
+SwSerial (10, 11); // RX, TX 
+
+dht DHT;
+int light=7;
+int water=2;
+char auth[] = "your api code";  
+
+BlynkTimer timer;
+void setup()
+{
+  // Debug console
+  SwSerial.begin(9600);
+  
+  // Blynk will work through Serial
+  // Do not read or write this serial manually in your sketch
+ 
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+  timer.run();
+}
+void myTimerEvent()
+{
+  int check=DHT.read11(dhtpin);
+  int sensor=DHT.temperature;
+  int humidity=DHT.humidity;
+  int soil=analogRead(A0);
+  Blynk.virtualWrite(V7,soil);
+ Blynk.virtualWrite(V5,sensor);
+ Blynk.virtualWrite(V6,humidity);
+
+}
